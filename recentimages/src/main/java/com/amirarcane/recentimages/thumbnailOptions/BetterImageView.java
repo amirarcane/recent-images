814f2a9b66392df7b5c6194768052ee8722cc40c
Amirhossein Naghshzan <n.naghshzan@gmail.com>
2017-07-03 17:24:32 +0430

    Revert "Update Gradle and Build Tools"

---
+++ recentimages/src/main/java/com/amirarcane/recentimages/thumbnailOptions/BetterImageView.java
@@ -0,0 +1,66 @@
+/*
+ * BetterImageView
+ *
+ * Copyright 2012 Jess Anders
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.amirarcane.recentimages.thumbnailOptions;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.ImageView;
+
+
+public class BetterImageView extends ImageView {
+	private static final String TAG = "BetterImageView";
+	private static final boolean DEBUG = false;
+
+	public BetterImageView(Context context) {
+		super(context);
+	}
+
+	public BetterImageView(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
+
+	public BetterImageView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	@Override
+	public void invalidateDrawable(Drawable dr) {
+		Drawable currentDrawable = getDrawable();
+		if (DEBUG) Log.i(TAG, "invalidateDrawable: " + dr + " current drawable: " + currentDrawable);
+		if (dr == currentDrawable) {
+			/* we invalidate the whole view in this case because it's very
+			 * hard to know where the drawable actually is. This is made
+			 * complicated because of the offsets and transformations that
+			 * can be applied. In theory we could get the drawable's bounds
+			 * and run them through the transformation and offsets, but this
+			 * is probably not worth the effort.
+			 */
+			Log.i(TAG, "invalidateDrawable - setting imageDrawable");
+			//destroyDrawingCache();
+			drawableStateChanged();
+			forceLayout();
+			setImageDrawable(currentDrawable);
+			invalidate();
+		} else {
+			super.invalidateDrawable(dr);
+		}
+	}
+}
---
+++ recentimages/src/main/java/com/amirarcane/recentimages/thumbnailOptions/ReplaceableBitmapDrawable.java
@@ -0,0 +1,118 @@
+/*
+ * ReplaceableBitmapDrawable
+ *
+ * Copyright 2012 Jess Anders
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.amirarcane.recentimages.thumbnailOptions;
+
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.view.Gravity;
+
+public class ReplaceableBitmapDrawable extends Drawable {
+	private static final String TAG = "ReplaceableBitmapDrawable";
+	private static final boolean DEBUG = false;
+
+	private Bitmap mBitmap;
+	private boolean mLoaded;
+	private boolean mApplyGravity;
+	private int mGravity;
+	private final Rect mDstRect = new Rect();
+
+	public ReplaceableBitmapDrawable(Bitmap b) {
+		mBitmap = b;
+	}
+
+	@Override
+	public void draw(Canvas canvas) {
+		copyBounds(mDstRect);
+		if (mBitmap != null) {
+			if (mApplyGravity) {
+				Gravity.apply(mGravity, super.getIntrinsicWidth(), super.getIntrinsicHeight(),
+						getBounds(), mDstRect);
+				mApplyGravity = false;
+			}
+			canvas.drawBitmap(mBitmap, null, mDstRect, null);
+
+		}
+	}
+
+
+	public void setGravity(int gravity) {
+		mGravity = gravity;
+		mApplyGravity = true;
+	}
+
+	@Override
+	public int getOpacity() {
+		return PixelFormat.OPAQUE;
+	}
+
+	@Override
+	public void setAlpha(int alpha) {
+	}
+
+	@Override
+	public void setColorFilter(ColorFilter cf) {
+	}
+
+	@Override
+	public int getIntrinsicWidth() {
+		if (mBitmap != null) {
+			return mBitmap.getWidth();
+		} else {
+			return 0;
+		}
+	}
+
+	@Override
+	public int getIntrinsicHeight() {
+		if (mBitmap != null) {
+			return mBitmap.getHeight();
+		} else {
+			return 0;
+		}
+	}
+
+	@Override
+	public int getMinimumWidth() {
+		return 0;
+	}
+
+	@Override
+	public int getMinimumHeight() {
+		return 0;
+	}
+
+	public Bitmap getBitmap() {
+		return mBitmap;
+	}
+
+	public void setBitmap(Bitmap bitmap) {
+		mLoaded = true;
+		mBitmap = bitmap;
+		invalidateSelf();
+	}
+
+	public boolean isLoaded() {
+		return mLoaded;
+	}
+}
